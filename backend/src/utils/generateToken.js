49f96ac7932a72dbb27a689d3f4434419981908d
Madhav Arora <aroramadhav2111@gmail.com>
2026-08-26 15:47:19 +0530

    Remove duplicate backend folder

--- backend/src/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-function generateToken(id) {
-    const token = jwt.sign({ id }, process.env.API_SECRET, { expiresIn: '15m' });
-    return token;
-}
-
-export default generateToken;
